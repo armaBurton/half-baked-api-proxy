183152543c5e69193386a8219428e3ea1819b4c9
armaBurton <armanarma@armaburton.com>
2022-02-15 20:44:46 -0800

    weather seach works but there is a bug. If you reload the page then everything breaks, I don't know how to fix it, I may remove it

---
+++ src/WeatherSpinner/WeatherSpinner.js
@@ -0,0 +1,5 @@
+export default function WeatherSpinner(){
+  return <div>
+    <img className='spinner' src="/weather.gif" alt="tina spinner" />
+  </div>;
+}
--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -41,4 +41,3 @@ export default function YelpSearch() {
     </section>
   );
 }
-  
